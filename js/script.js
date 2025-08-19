e961980fa2a057182e1f66a10ce35241cbbd55a2
unknown <jeferson_bernardes@terra.com.br>
2025-08-18 21:53:46 -0300

    feat: atualizando URL do backend para Heroku

--- js/script.js
+++ js/script.js
@@ -417,7 +417,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     try {
         console.log('3. Tentando enviar a requisição para o backend...');
-        const response = await fetch('http://localhost:3000/enviar-ficha', {
+        const response = await fetch('https://ficha-locacao-e70b43034247.herokuapp.com/', {
             method: 'POST',
             body: formData,
         });
